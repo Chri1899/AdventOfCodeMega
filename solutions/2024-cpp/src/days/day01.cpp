92c11f508f41c9a242b98cad59819124c727a63b
Chris B. <97324676+Chri1899@users.noreply.github.com>
2025-06-08 00:04:57 +0200

    Sample day01

---
+++ solutions/2024-cpp/src/days/day01.cpp
@@ -0,0 +1,12 @@
+//
+// Created by cb-1 on 08/06/2025.
+//
+
+#include "day01.h"
+#include "..utils/readfile.h"
+#include <iostream>
+
+void Day01::run() {
+    auto lines = read_file_lines("data/2024/test.txt");
+    std::cout << "Day 01: Input lines = " << lines.size() << "\n";
+}
---
+++ solutions/2024-cpp/src/days/day01.h
@@ -0,0 +1,11 @@
+//
+// Created by cb-1 on 07/06/2025.
+//
+
+#pragma once
+#include "IDay.h"
+
+class Day01 : public IDay {
+public:
+    void run() override;
+};
