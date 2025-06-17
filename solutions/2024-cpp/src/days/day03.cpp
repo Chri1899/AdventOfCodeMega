5259ac7ddac9d1a377b052edd7be4d450423be57
Chris B. <97324676+Chri1899@users.noreply.github.com>
2025-06-17 13:49:03 +0200

    Day03 part two

--- solutions/2024-cpp/src/days/day03.cpp
+++ solutions/2024-cpp/src/days/day03.cpp
@@ -19,17 +19,15 @@ std::string Day03::part_one(const std::vector<std::string>& input) {
         // Process each string in the input vector
         for (const auto& line : input) {
             // Set the iterator to the beginning of the string
-            std::string::const_iterator searchStart(line.cbegin());
+            std::string::const_iterator begin(line.cbegin());
             // Search in the string for the regex pattern
-            while (std::regex_search(searchStart, line.cend(), match, pattern)) {
+            while (std::regex_search(begin, line.cend(), match, pattern)) {
                 int x = std::stoi(match[1].str());
                 int y = std::stoi(match[2].str());
                 total += x * y;
-                searchStart = match.suffix().first;
+                begin = match.suffix().first;
             }
         }
-
-        std::cout << "Total: " << total << "\n";
         return std::to_string(total);
     } catch (const std::runtime_error& e) {
         return std::string("Error: ") + e.what();
@@ -39,5 +37,38 @@ std::string Day03::part_one(const std::vector<std::string>& input) {
 std::string Day03::part_two(const std::vector<std::string>& input) {
     if (input.empty()) return "Empty input";
 
-    return "Not implemented yet.";
+    try {
+        // Mul pattern
+        std::regex token_pattern(R"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))");
+        std::smatch match;
+        int total = 0;
+
+        // Mul instruction enabled by default
+        bool mul_enabled = true;
+
+        // Iterate over the input and process each single line
+        for (const auto& line : input) {
+            // Iterator
+            std::string::const_iterator begin(line.cbegin());
+
+            while (std::regex_search(begin, line.cend(), match, token_pattern)) {
+                std::string token = match[0].str();
+
+                if (token == "do()") {
+                    mul_enabled = true;
+                } else if (token == "don't()") {
+                    mul_enabled = false;
+                } else if (match[1].matched && match[2].matched && mul_enabled) {
+                    int x = std::stoi(match[1].str());
+                    int y = std::stoi(match[2].str());
+                    total += x * y;
+                }
+
+                begin = match.suffix().first;
+            }
+        }
+        return std::to_string(total);
+    } catch (const std::runtime_error& e) {
+        return std::string("Error: ") + e.what();
+    }
 }
